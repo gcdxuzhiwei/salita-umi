f94c54f32d20444539299f30d1d152b4860debe8
xuzhiwei <xuzhiwei4347@qq.com>
2021-01-13 23:40:00 +0800

    no message

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios'; // 该处引入axios模块
-import {getUmiCookie} from './const'
+import { getUmiCookie } from './const';
 
 // 构建axios实例
 const instance = axios.create({});
 
 instance.interceptors.response.use(config => {
-  if(getUmiCookie()){
-    axios.post('/api/user/setAction')
+  if (getUmiCookie()) {
+    axios.post('/api/user/setAction');
   }
   return config;
 });
