3e09e4a0d3a59cad20585b48ab2dac91c77d12ce
xuzhiwei <gcdxuzhiwei@gmail.com>
2021-02-26 18:21:36 +0800

    join field

--- src/pages/join/index.jsx
+++ src/pages/join/index.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'umi';
-import { Spin } from 'antd';
+import { Spin, Upload } from 'antd';
 import { Steps, WhiteSpace, Toast, SegmentedControl } from 'antd-mobile';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import axios from '@/utils/axios';
 import styles from './index.less';
 
@@ -25,10 +26,36 @@ const steps = [
 function Join() {
   const [state, setState] = useState(-1);
   const [loading, setLoading] = useState(true);
+  const [isTeacher, setIsTeacher] = useState(0);
+  const [imageLoading, setImageLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState('');
 
   useEffect(() => {
     getState();
   }, []);
+
+  const beforeUpload = file => {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('只能上传JPG/PNG图片!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('图片大小不能超过2MB!');
+    }
+    return isJpgOrPng && isLt2M;
+  };
+
+  const avatarChange = info => {
+    if (info.file.status === 'uploading') {
+      setImageLoading(true);
+      return;
+    }
+    if (info.file.status === 'done') {
+      setImageUrl(info.file.response.url[0]);
+      setImageLoading(false);
+    }
+  };
 
   const getState = async () => {
     try {
@@ -59,7 +86,34 @@ function Join() {
         <div className={styles.field}>
           <div className={styles.role}>
             教员身份:
-            <SegmentedControl values={['在校学生', '在职教师']} />
+            <SegmentedControl
+              values={['在校学生', '在职教师']}
+              selectedIndex={isTeacher}
+              onChange={e => {
+                setIsTeacher(e.nativeEvent.selectedSegmentIndex);
+              }}
+            />
+          </div>
+          <div className={styles.photo}>
+            手持{isTeacher ? '教师' : '学生'}证件照片:
+            <Upload
+              name="avatar"
+              showUploadList={false}
+              action="/api/upload"
+              beforeUpload={beforeUpload}
+              onChange={avatarChange}
+            >
+              <div className={styles.upload}>
+                {imageUrl ? (
+                  <img src={'/public/' + imageUrl} alt="avatar" />
+                ) : (
+                  <div className={styles.icon}>
+                    {imageLoading ? <LoadingOutlined /> : <PlusOutlined />}
+                    <div>Upload</div>
+                  </div>
+                )}
+              </div>
+            </Upload>
           </div>
         </div>
       )}
