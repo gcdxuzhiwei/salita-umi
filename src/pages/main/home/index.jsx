7c8312de48ce155ec2bcbdd8e9a9371b4ecec39d
xuzhiwei <gcdxuzhiwei@gmail.com>
2021-03-04 09:39:44 +0800

    change host

--- src/pages/main/home/index.jsx
+++ src/pages/main/home/index.jsx
@@ -5,7 +5,7 @@ import { GithubOutlined } from '@ant-design/icons';
 function Home() {
   return (
     <NoticeBar marqueeProps={{ loop: true }}>
-      沪ICP备20006702号-1 毕设项目，非真实业务!!!
+      沪ICP备20006702号-2 毕设项目，非真实业务!!!
       <a href="https://github.com/gcdxuzhiwei" target="_blank">
         源码点击跳转github
         <GithubOutlined />
