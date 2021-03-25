03300d179a5b4f85a9d4bb90dea23e184a644bb8
xuzhiwei <gcdxuzhiwei@gmail.com>
2021-03-26 00:11:33 +0800

    no message

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Skeleton, Card, Avatar } from 'antd';
-import { CommentOutlined, CarryOutOutlined } from '@ant-design/icons';
-import { Toast } from 'antd-mobile';
+import {
+  CommentOutlined,
+  CarryOutOutlined,
+  CheckCircleOutlined,
+} from '@ant-design/icons';
+import { Toast, List, TextareaItem } from 'antd-mobile';
+import moment from 'moment';
 import axios from '@/utils/axios';
 import styles from './index.less';
 
@@ -9,6 +14,7 @@ const { Meta } = Card;
 
 function Detail(props) {
   const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState(null);
 
   useEffect(() => {
     getData(...props.history.location.pathname.split('/').slice(-1));
@@ -16,7 +22,15 @@ function Detail(props) {
 
   const getData = async id => {
     try {
-      const { data } = await axios.post('/api/user/userDetail');
+      const { data } = await axios.post('/api/user/userDetail', {
+        userId: id,
+      });
+      if (data.retdata) {
+        setInfo(data.retdata);
+        setLoading(false);
+      } else {
+        Toast.fail(data.err);
+      }
     } catch {
       Toast.fail('网络异常');
     }
@@ -28,25 +42,110 @@ function Detail(props) {
         className={styles.card}
         style={{ width: '100%', height: '100%' }}
         bordered={false}
-        actions={[
-          <>
-            <CommentOutlined />
-            联系TA
-          </>,
-          <>
-            <CarryOutOutlined />
-            预约课程
-          </>,
-        ]}
+        actions={
+          !loading && [
+            <>
+              <CommentOutlined />
+              联系TA
+            </>,
+            <>
+              <CarryOutOutlined />
+              预约课程
+            </>,
+          ]
+        }
       >
-        <Skeleton loading={false} avatar active>
-          <Meta
-            avatar={
-              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-            }
-            title="Card title"
-            description="This is the description"
-          />
+        <Skeleton loading={loading} avatar active>
+          {!loading && (
+            <>
+              <Meta
+                avatar={<Avatar src={`/public/${info.avatar}`} />}
+                title={
+                  <>
+                    {info.name}
+                    <span
+                      style={{
+                        marginLeft: 8,
+                        fontWeight: 'normal',
+                        opacity: 0.9,
+                      }}
+                    >
+                      {info.age}岁
+                    </span>
+                  </>
+                }
+                description={
+                  <div
+                    style={{ display: 'flex', justifyContent: 'space-between' }}
+                  >
+                    <span>
+                      <CheckCircleOutlined
+                        style={{ color: '#0084c9', marginRight: 8 }}
+                      />
+                      认证{info.isTeacher ? '专职教师' : '在校学生'}
+                    </span>
+                    <span style={{ marginLeft: 'auto' }}>
+                      浏览量:{info.visitCount}
+                    </span>
+                  </div>
+                }
+              />
+              <List renderHeader="基本信息">
+                <List.Item
+                  extra={
+                    <>
+                      {info.group1.split('').map(v => (
+                        <span
+                          key={v}
+                          style={{ backgroundColor: 'greenyellow' }}
+                        >
+                          {v}
+                        </span>
+                      ))}
+                    </>
+                  }
+                >
+                  可授年级
+                </List.Item>
+                <List.Item
+                  extra={
+                    <>
+                      {info.group2.split('').map(v => (
+                        <span key={v} style={{ backgroundColor: '#0084c9' }}>
+                          {v}
+                        </span>
+                      ))}
+                    </>
+                  }
+                >
+                  可授课程
+                </List.Item>
+                <List.Item extra={info.area}>地区</List.Item>
+                <List.Item extra={moment(info.time).fromNow()}>
+                  上次活跃
+                </List.Item>
+                <List.Item extra={info.school}>学校</List.Item>
+                <List.Item extra={info.profession}>专业</List.Item>
+                <List.Item extra={info.level === 1 ? '研究生' : '本科'}>
+                  学历
+                </List.Item>
+              </List>
+              <List renderHeader="自我介绍" className="antbody">
+                <pre>
+                  {info.introduce +
+                    info.introduce +
+                    info.introduce +
+                    info.introduce +
+                    info.introduce +
+                    info.introduce +
+                    info.introduce +
+                    info.introduce +
+                    info.introduce +
+                    info.introduce}
+                </pre>
+              </List>
+            </>
+          )}
         </Skeleton>
       </Card>
     </>
