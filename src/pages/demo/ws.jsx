bef82a324abab08371a6a0ecfa3499db7375e3c7
xuzhiwei <gcdxuzhiwei@gmail.com>
2021-04-12 07:43:34 +0800

    no message

--- src/pages/demo/ws.jsx
+++ src/pages/demo/ws.jsx
@@ -7,22 +7,25 @@ const { Item } = List;
 let socket;
 
 function SampleComponent() {
+  const [login, setLogin] = useState(false);
   const [name, setName] = useState('');
   const [chat, setChat] = useState('');
   const [detail, setDetail] = useState([]);
 
   useEffect(() => {
-    socket = io(`ws://${location.hostname}:3333`, {
-      transports: ['websocket'],
-    });
-    socket.emit('room', 'demo');
-    socket.on('chat', res => {
-      setDetail(res);
-    });
-    socket.on('addChat', e => {
-      setDetail(v => [e, ...v]);
-    });
-  }, []);
+    if (login) {
+      socket = io(`ws://${location.hostname}:3333`, {
+        transports: ['websocket'],
+      });
+      socket.emit('room', login);
+      socket.on('chat', res => {
+        setDetail(res);
+      });
+      socket.on('addChat', e => {
+        setDetail(v => [e, ...v]);
+      });
+    }
+  }, [login]);
 
   const handleClick = () => {
     if (!name || !chat) {
@@ -32,20 +35,43 @@ function SampleComponent() {
     socket.emit('add', {
       name,
       chat,
+      room: login,
     });
     setChat('');
   };
 
   return (
     <>
-      <InputItem placeholder="用户名" value={name} onChange={setName} />
-      <InputItem placeholder="内容" value={chat} onChange={setChat} />
-      <Button onClick={handleClick}>发送</Button>
-      {detail.map(v => (
-        <Item key={v.name + v.chat + Math.random()}>
-          {v.name} : {v.chat}
-        </Item>
-      ))}
+      {!login && (
+        <>
+          <Button
+            onClick={() => {
+              setLogin('demo');
+            }}
+          >
+            demo
+          </Button>
+          <Button
+            onClick={() => {
+              setLogin('ws');
+            }}
+          >
+            ws
+          </Button>
+        </>
+      )}
+      {login && (
+        <>
+          <InputItem placeholder="用户名" value={name} onChange={setName} />
+          <InputItem placeholder="内容" value={chat} onChange={setChat} />
+          <Button onClick={handleClick}>发送</Button>
+          {detail.map(v => (
+            <Item key={v.name + v.chat + Math.random()}>
+              {v.name} : {v.chat}
+            </Item>
+          ))}
+        </>
+      )}
     </>
   );
 }
